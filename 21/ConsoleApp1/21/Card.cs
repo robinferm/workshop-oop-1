ecdb83fda3a17e4ba3ca406415d77f36f62149ec
Farhad Tavakoli <farhad.tavakoli81@yahoo.com>
2021-02-24 13:19:59 +0100

    Playes and Card classes

---
+++ 21/ConsoleApp1/21/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21
+{
+    class Card
+    {
+        enum suits
+        {
+            clubs,
+            diamonds,
+            hearts,
+            spades
+        }
+        enum values
+        {
+            ace,
+            two,
+            three,
+            four,
+            five,
+            six,
+            seven,
+            eight,
+            nine,
+            ten,
+            jack,
+            queen,
+            king
+        }
+
+
+
+
+    }
+}
